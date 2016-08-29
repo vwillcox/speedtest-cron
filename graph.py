816a463b7c44372743b142ca0f2101016aacd2cd
cmcconomy <cmcconomy@gmail.com>
2016-08-28 23:55:16 -0400

    Updated graph.py
    Now reads only from speedtest.txt
    Encapsulated more functions

--- graph.py
+++ graph.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python
 
-from glob import glob
 from io import open
 from datetime import datetime
 from ascii_graph import Pyasciigraph
+
+def read_speedtest(file):
+  line = file.readline()
+  if line == '':
+    return None
+  datetime_line = line.split()
+  speedtest = {}
+  speedtest['datetime'] = datetime.strptime(datetime_line[4] + ' ' + datetime_line[5], '%d/%m/%Y %H:%M')
+  speedtest['ping'] = float(file.readline().split()[1])
+  speedtest['download'] = float(file.readline().split()[1])
+  speedtest['upload'] = float(file.readline().split()[1])
+  file.readline() # to consume the '----------' separator
+  return speedtest
 
 def to_graph_input(speedtests, attr):
   "Takes speedtest list and extracts the attribute, suitable for input into Pyasciigraph"
   return map(lambda speedtest: (datetime.strftime(speedtest['datetime'], '%d/%m/%Y %H:%M'), speedtest[attr]), speedtests)
 
+def print_graph(speedtests, dict_entry, title, unit):
+  graph = Pyasciigraph(line_length=60, min_graph_length=30, graphsymbol='=', float_format='{0:,.2f} ' + unit)
+  for line in graph.graph(title, to_graph_input(speedtests,dict_entry)):
+    print line
+  print
 # ------ main -------
-
-filenames = glob("./*speedtest.txt")
 
 speedtests=[]
 
-for filename in filenames:
-  with open(filename,'r') as file:
-    datetime_line = file.readline().split()
-    speedtest = {}
-    speedtest['datetime'] = datetime.strptime(datetime_line[4] + ' ' + datetime_line[5], '%d/%m/%Y %H:%M')
-    speedtest['ping'] = float(file.readline().split()[1])
-    speedtest['download'] = float(file.readline().split()[1])
-    speedtest['upload'] = float(file.readline().split()[1])
+with open('speedtest.txt','r') as file:
+  speedtest = read_speedtest(file)
+  while speedtest != None:
     speedtests.append(speedtest)
+    speedtest = read_speedtest(file)
 
-speedtests = sorted(speedtests, key=lambda speedtest: speedtest['datetime'])
+#speedtests = sorted(speedtests, key=lambda speedtest: speedtest['datetime'])
 
-graph = Pyasciigraph(float_format='{0:,.2f} ms')
-for line in graph.graph('Ping (ms)', to_graph_input(speedtests,'ping')):
-  print line
+print 'Speedtest results for period from ' + datetime.strftime(speedtests[0]['datetime'], '%d/%m/%Y %H:%M') + ' to ' + datetime.strftime(speedtests[-1]['datetime'], '%d/%m/%Y %H:%M')
+print
 print
 
-graph = Pyasciigraph(float_format='{0:,.2f} Mbps')
-for line in graph.graph('Download (Mbps)', to_graph_input(speedtests,'download')):
-  print line
-print
-
-for line in graph.graph('Upload (Mbps)', to_graph_input(speedtests,'upload')):
-  print line
-print
+print_graph(speedtests, 'ping', 'Ping (ms)', 'ms')
+print_graph(speedtests, 'download', 'Download (Mbps)', 'Mbps')
+print_graph(speedtests, 'upload', 'Upload (Mbps)', 'Mbps')
