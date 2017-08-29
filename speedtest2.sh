9d01807f79c0dbf5b9ea0f09f5b322f4ccc0c86b
vwillcox <talking@talktech.info>
2017-08-29 16:43:26 +0000

    Button to show a graph for up0load and download]

--- speedtest2.sh
+++ speedtest2.sh
@@ -3,7 +3,8 @@
 OUTPUT=`cat speedtest.txt`
 DOWNLOAD=`echo "$OUTPUT" | grep Download | sed 's/[a-zA-Z:]* \([0-9]*\.[0-9]*\) [a-zA-Z/]*/\1/'`
 UPLOAD=`echo "$OUTPUT" | grep Upload | sed 's/[a-zA-Z:]* \([0-9]*\.[0-9]*\) [a-zA-Z/]*/\1/'`
-TIME=`echo "$OUTPUT" | grep at | sed 's/[a-zA-Z:]* \([0-9]*\.[0-9]*\) [a-zA-Z/]*/\1/'`
+#TIME=`echo "$OUTPUT" | grep at | sed 's/[a-zA-Z:]* \([0-9]*\.[0-9]*\) [a-zA-Z/]*/\1/'`
 
 echo "$DOWNLOAD" > graph.txt
+echo "$UPLOAD" > graph2.txt
 #echo "up.value $UPLOAD"
