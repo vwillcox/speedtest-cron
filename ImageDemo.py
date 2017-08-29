398c4d3860661978d17f59d951bb52daf16ead89
vwillcox <talking@talktech.info>
2017-08-29 17:08:09 +0000

    combined graph2image.py and ImageDemo.py into one file for speed

--- ImageDemo.py
+++ ImageDemo.py
@@ -20,10 +20,37 @@ import time
 from PIL import Image
 from PIL import ImageOps
 from EPD import EPD
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import subprocess
+import shlex
+
+
 
 
 def main(argv):
     """main program - display list of images"""
+    subprocess.call(shlex.split('/home/pi/speedtest-cron/speedtest2.sh'))
+    x = 0
+    with open('graph.txt') as f:
+        lines = f.readlines()
+        y = [line.split()[0] for line in lines]
+
+    with open('graph2.txt') as f2:
+        lines = f2.readlines()
+        y2 = [line.split()[0] for line in lines]
+        #x = [x++1 for line in lines]
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.set_title("Speed Test")
+    ax1.plot(y, c='r', label='Down')
+    ax1.plot(y2, c='b', label='Up')
+    leg = ax1.legend()
+    fig.set_size_inches(3.8, 2.5)
+    fig.savefig('temp.png', dpi=75)
+
 
     epd = EPD()
 
