d02410216b7fcd66c6dcd8ae30f6156e25f6c9d1
JarradStarr <jarrad.starr@bulletproof.net>
2017-11-10 22:31:22 +1100

    remove unnecessary dependencies

--- install.sh
+++ install.sh
@@ -23,8 +23,8 @@ function pip_install {
 
 echo "Welcome to The Network Testing Appliance Installer"
 pause 'Press [Enter] to install the required bits and pieces or CTRL+C to stop...'
-echo "Installing mpack zip ssmtp mailutils mpack python-pip python3-pip python-w1thermsensor python3-w1thermsensor python-spidev python3-spidev"
-apt-get install mpack zip ssmtp mailutils mpack python-pip python3-pip python-w1thermsensor python3-w1thermsensor python-spidev python3-spidev -y 2>&1 >/dev/null
+echo "Installing mpack zip ssmtp mailutils mpack python-pip python3-pip python-spidev python3-spidev"
+apt-get install mpack zip ssmtp mailutils mpack python-pip python3-pip python-spidev python3-spidev -y 2>&1 >/dev/null
 if [ "$?" -ne 0 ]; then
   echo "Error while running apt-get (maybe run apt-get update?)";
   exit 1;
